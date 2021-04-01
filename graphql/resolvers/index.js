56bbf06192def43b52424e87b21e111dbe6ff240
hareesh43 <hareeshgummani@gmail.com>
2021-04-01 15:57:26 +0530

    added likes and comments to posts

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -1,5 +1,7 @@
 const postResolvers = require("./posts");
 const userResolvers = require("./users");
+const commentsResolvers = require("./comments");
+
 
 module.exports = {
   Query: {
@@ -8,5 +10,6 @@ module.exports = {
   Mutation: {
     ...userResolvers.Mutation,
     ...postResolvers.Mutation,
+    ...commentsResolvers.Mutation
   },
 };
