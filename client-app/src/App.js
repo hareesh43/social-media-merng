f665fb736c5913f2b68848cf4549ab04e0411fcd
hareesh43 <hareeshgummani@gmail.com>
2021-04-05 10:25:19 +0530

    added routing path

--- client-app/src/App.js
+++ client-app/src/App.js
@@ -1,11 +1,25 @@
-
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import "semantic-ui-css/semantic.min.css";
+import { Container } from "semantic-ui-react";
 import "./App.css";
+import MenuBar from "./component/MenuBar";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
   return (
-    <div className="App">
-      <h1>HI There</h1>
-    </div>
+    <Router>
+      <Container>
+        <MenuBar />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/register" component={Register} />
+        </Switch>
+      </Container>
+    </Router>
   );
 }
 
