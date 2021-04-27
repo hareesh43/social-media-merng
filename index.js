77ff4f55acd605d8749d54a93d43a34438e935c4
hareesh43 <hareeshgummani@gmail.com>
2021-04-27 11:53:25 +0530

    minor fix on server port

--- index.js
+++ index.js
@@ -1,23 +1,27 @@
-const { ApolloServer,PubSub } = require("apollo-server");
+const { ApolloServer, PubSub } = require("apollo-server");
 const mongoose = require("mongoose");
 
 const { MONGODB } = require("./config");
 const resolvers = require("./graphql/resolvers");
 const typeDefs = require("./graphql/typeDefs");
 
-const pubsub = new PubSub()
+const PORT = process.env.PORT || 3000
+const pubsub = new PubSub();
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: ({ req }) => ({ req,pubsub }),
+  context: ({ req }) => ({ req, pubsub }),
 });
 
 mongoose
   .connect(MONGODB, { useNewUrlParser: true })
   .then(() => {
-    return server.listen({ port: 3000 });
+    return server.listen({ port: PORT });
   })
   .then((res) => {
     console.log(`server is listening on ${res.url}`);
+  })
+  .catch((error) => {
+    console.log(error);
   });
